a0c0505f9a5f5b3be59e62a063b96e9a992d446e
vinceent <jonanv@uw.edu>
2014-10-28 17:33:21 -0700

    CLOSEEgit config --global user.name vinceent

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-//to do: Reset Button.
-//        put the tile back to inital when no mathced pairs
-// if matched, make them stay opened
-//check challenge descruption.
+//TO DO: check line 114
+// Add share to FB
+// add feedbacks or rules or instruction
+// id users win?
 
-$(document).ready(function() {
+$(document).ready(startGame);
+
+function startGame() {
     var tiles = [];
-    for(var i = 1; i <= 32; i++) {
+    for (var i = 1; i <= 32; i++) {
         tiles.push({
             tileNum: i,
             src: 'img/tile' + i + '.jpg',
@@ -17,7 +19,7 @@ $(document).ready(function() {
 
     var selectedTiles = shuffledTiles.slice(0, 8);
     var pairedTiles = [];
-    _.forEach(selectedTiles, function(tile) {
+    _.forEach(selectedTiles, function (tile) {
         pairedTiles.push(_.clone(tile));
         pairedTiles.push(_.clone(tile));
     });
@@ -27,134 +29,129 @@ $(document).ready(function() {
     var gameBoard = $('#game-board');
     var row = $(document.createElement('div'));
     var img;
-    _.forEach(pairedTiles, function(tile, elemIndex) {
-        if(elemIndex > 0 && 0 == elemIndex % 4) {
+    _.forEach(pairedTiles, function (tile, elemIndex) {
+        if (elemIndex > 0 && 0 == elemIndex % 4) {
             gameBoard.append(row);
             row = $(document.createElement('div'));
         }
-
         img = $(document.createElement('img'));
         img.attr({
             src: 'img/tile-back.png',
             alt: 'image of tile ' + tile.tileNum
-
         });
         img.data('tile', tile);
         row.append(img);
     });
     gameBoard.append(row);
 
-
     $('#resetButton').hide();
 
-    $('#startButton').click(function() {
+    $('#startButton').click(onClick);
 
-        var matchedPairs = 0;
-        var wrongPairs = 0;
-        var remainingPairs = 0;
-        var keyToCheck;
+    $('#resetButton').click(function () {
 
-        $('#matches').text('Matched: ' + matchedPairs);
-        $('#mistakes').text('Mistakes: ' + wrongPairs);
+        $('#game-board').empty();
+
+        matchedPairs = 0;
+        wrongPairs = 0;
+        remainingPairs = 8;
+
         $('#remaining').text('Remaining: ' + remainingPairs);
-
-        $('#resetButton').show();
-        $('#startButton').hide();
-        var startTime = _.now();
-
-        timer = window.setInterval(function() {
-            var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
-            if(elapsedSeconds > 100) {
-                window.clearInterval(timer);
-            }
-            $('#elapsedSeconds').text('Elapsed Time: ' + elapsedSeconds);
-
-
-        }, 1000);
-
-        var past;
-
-        $('#game-board img').click(function() {
-            var img = $(this);
-            var tile = img.data('tile');
-            console.log(tile.clicked); // chck if it is clicked
-            img.fadeIn(100, function() {
-                if(tile.clicked == false) {
-                    img.attr('src', tile.src);
-//                    tile.clicked = true;
-                }
-
-            }); //fade effects
-
-
-            if(!past) {
-                past = img;
-            }
-
-
-
-            //THIS NEEDS MORE DEBUGGING!
-            else if (past[0] != img[0]) {
-                console.log(tile.clicked);
-                if((past[0].src == img[0].src) && (tile.clicked == false)) { // NEED HELP IN THIS!
-                    matchedPairs += 1;
-                    $('#matches').text('Matched: ' + matchedPairs);
-                } //match found
-
-                else {
-
-                    console.log('WRONG!');
-                    var prev = past;
-
-                    prev.fadeOut(100, function() {
-                        prev.attr('src', 'img/tile-back.png');
-                        prev.clicked = false;
-                    });
-                    prev.fadeIn(100);
-
-                    img.fadeOut(100, function() {
-                        img.attr('src', 'img/tile-back.png');
-                        tile.clicked = false;
-                    });
-                    img.fadeIn(100);
-
-                } //wrong pair
-
-                past = null;
-            }
-
-//
-
-            tile.clicked = true;
-
-
-
-
-        }); //when Images are clicked
-
-
-
-
-    });
-
-    $('#resetButton').click(function() {
-        resetTimer(); //reset the timer
-        //reset the images? how?!
-    }); //when reset button is clicked
-
-    function resetTimer() {
+        $('#mistakes').text('Mistakes: ' + wrongPairs);
+        $('#matches').text('Matched: ' + matchedPairs);
         window.clearInterval(timer);
-        var startTime = _.now();
-        timer = window.setInterval(function () {
-            var elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
-            if (elapsedSeconds > 10) {
-                window.clearInterval(timer);
+        startGame();
+        $('#resetButton').show();
+        onClick();
+
+    }); //when reset button is clicked
+}
+
+
+function onClick() {
+
+    $('#matches').text('Matched: ' + matchedPairs);
+    $('#mistakes').text('Mistakes: ' + wrongPairs);
+    $('#remaining').text('Remaining: ' + remainingPairs);
+
+    $('#resetButton').show();
+    $('#startButton').hide();
+    var startTime = _.now();
+
+    timer = window.setInterval(function () {
+        elapsedSeconds = Math.floor((_.now() - startTime) / 1000);
+        $('#elapsedSeconds').text('Elapsed Time: ' + elapsedSeconds);
+
+
+    }, 1000);
+
+    var past;
+
+    $('#game-board img').click(function () {
+        var img = $(this);
+        var tile = img.data('tile');
+        img.fadeIn(100, function () {
+            if (tile.clicked == false) {
+                img.attr('src', tile.src);
+                tile.clicked = true;
             }
-            $('#elapsedSeconds').text('Elapsed Time: ' + elapsedSeconds);
-        }, 1000);
-    }
 
-});
+        }); //fade effects
 
 
+        if (!past) {
+            past = img;
+        }
+
+        //THIS NEEDS MORE DEBUGGING!
+        else if (past[0] != img[0]) {
+
+            if ((past[0].src == img[0].src)) {
+
+                matchedPairs++;
+                $('#matches').text('Matched: ' + matchedPairs);
+                remainingPairs = 8 - matchedPairs;
+                if(remainingPairs == 0 || matchedPairs == 8) {
+                    window.clearInterval(timer);
+                }
+                // Does not allow any more clicks here ?!!! how?!!
+
+            } //match found
+
+            else {
+
+                var prev = past;
+                var prevTile = prev.data('tile');
+
+                prev.fadeOut(100, function () {
+                    prev.attr('src', 'img/tile-back.png');
+                    prevTile.clicked = false;
+                });
+                prev.fadeIn(100);
+
+                img.fadeOut(100, function () {
+                    img.attr('src', 'img/tile-back.png');
+                    tile.clicked = false;
+                });
+                img.fadeIn(100);
+
+                wrongPairs++;
+                $('#mistakes').text('Mistakes: ' + wrongPairs);
+
+
+            } //wrong pair
+
+            $('#remaining').text('Remaining: ' + remainingPairs);
+            past = null;
+        }
+
+    }); //when Images are clicked
+}
+
+
+
+var matchedPairs = 0;
+var wrongPairs = 0;
+var remainingPairs = 8;
 var timer; //global variable to set the timer back to 0 again.
+var elapsedSeconds;
