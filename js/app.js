ed4c7412dfe33ee346cbc7bb833c5eaa3e6f5965
vincent <jonanv@uw.edu>
2014-10-29 12:15:18 -0700

    flipping == true?

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 //TO DO: check line 114
-// Add share to FB
-// add feedbacks or rules or instruction
-// id users win?
+// add feedbacks or rules or instruction.
+// if users win? Give sort of a congratulation message, but still show the start over button.
 
 $(document).ready(startGame);
-
 function startGame() {
+
     var tiles = [];
     for (var i = 1; i <= 32; i++) {
         tiles.push({
             tileNum: i,
             src: 'img/tile' + i + '.jpg',
-            clicked: false
+            clicked: false,
+            matched: false // new property
         });
     }
 
@@ -19,6 +19,7 @@ function startGame() {
 
     var selectedTiles = shuffledTiles.slice(0, 8);
     var pairedTiles = [];
+
     _.forEach(selectedTiles, function (tile) {
         pairedTiles.push(_.clone(tile));
         pairedTiles.push(_.clone(tile));
@@ -27,8 +28,10 @@ function startGame() {
     pairedTiles = _.shuffle(pairedTiles);
 
     var gameBoard = $('#game-board');
+
     var row = $(document.createElement('div'));
     var img;
+
     _.forEach(pairedTiles, function (tile, elemIndex) {
         if (elemIndex > 0 && 0 == elemIndex % 4) {
             gameBoard.append(row);
@@ -42,6 +45,7 @@ function startGame() {
         img.data('tile', tile);
         row.append(img);
     });
+
     gameBoard.append(row);
 
     $('#resetButton').hide();
@@ -88,6 +92,11 @@ function onClick() {
     var past;
 
     $('#game-board img').click(function () {
+
+//        if (flipping == true) {
+//            alert();
+//        }
+
         var img = $(this);
         var tile = img.data('tile');
         img.fadeIn(100, function () {
@@ -95,9 +104,11 @@ function onClick() {
                 img.attr('src', tile.src);
                 tile.clicked = true;
             }
-
         }); //fade effects
 
+        if (tile.matched == true) {
+            return;
+        }
 
         if (!past) {
             past = img;
@@ -105,8 +116,10 @@ function onClick() {
 
         //THIS NEEDS MORE DEBUGGING!
         else if (past[0] != img[0]) {
+            var prev = past;
+            var prevTile = prev.data('tile');
 
-            if ((past[0].src == img[0].src)) {
+            if (past[0].src == img[0].src) {
 
                 matchedPairs++;
                 $('#matches').text('Matched: ' + matchedPairs);
@@ -114,42 +127,45 @@ function onClick() {
                 if(remainingPairs == 0 || matchedPairs == 8) {
                     window.clearInterval(timer);
                 }
-                // Does not allow any more clicks here ?!!! how?!!
+                prevTile.matched = true;
+                tile.matched = true;
 
             } //match found
 
             else {
 
-                var prev = past;
-                var prevTile = prev.data('tile');
+                setTimeout(function() {
+//                    flipping = true;
+                    prev.fadeOut(100, function () {
+                        prev.attr('src', 'img/tile-back.png');
+                        prevTile.clicked = false;
+                    });
+                    prev.fadeIn(100);
 
-                prev.fadeOut(100, function () {
-                    prev.attr('src', 'img/tile-back.png');
-                    prevTile.clicked = false;
-                });
-                prev.fadeIn(100);
+                    img.fadeOut(100, function () {
+                        img.attr('src', 'img/tile-back.png');
+                        tile.clicked = false;
+                    });
+                    img.fadeIn(100);
 
-                img.fadeOut(100, function () {
-                    img.attr('src', 'img/tile-back.png');
-                    tile.clicked = false;
-                });
-                img.fadeIn(100);
+                }, 1000);
 
+//                flipping = false;
                 wrongPairs++;
                 $('#mistakes').text('Mistakes: ' + wrongPairs);
-
 
             } //wrong pair
 
             $('#remaining').text('Remaining: ' + remainingPairs);
             past = null;
+
         }
 
     }); //when Images are clicked
 }
 
 
-
+//var flipping = false;
 var matchedPairs = 0;
 var wrongPairs = 0;
 var remainingPairs = 8;
